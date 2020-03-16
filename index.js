42f32c53e74733dad48f5fde1a6287a0313a13ab
austinoso <osorio_austin@yahoo.com>
2020-03-16 14:39:20 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  const game = array.find(game => game.result === "W")
+  return game ? game.year : undefined
+}
